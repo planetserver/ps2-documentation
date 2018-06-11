e922d638375ed3d39117bae55cd6a402faa6622a
Mikhail Minin <mminin2010@gmail.com>
2018-06-11 13:05:53 +0200

    Update getEWLon.sh

--- developer_documentation/scripts/getEWLon.sh
+++ developer_documentation/scripts/getEWLon.sh
@@ -1,5 +1,11 @@
-wget "http://oderest.rsl.wustl.edu/live2/?target=mars&query=product&results=m&output=XML&pt=TRDRDDR&iid=CRISM&ihid=MRO&proj=c0&westlon=0&eastlon=360&minlat=-90&maxlat=90&pdsid=$1" -O temp.xml > /dev/null 2>&1
-ELon=($(grep -oP '(?<=Easternmost_longitude>)[^<]+' "temp.xml"))
-WLon=($(grep -oP '(?<=Westernmost_longitude>)[^<]+' "temp.xml"))
-echo $1 $ELon-$WLon
-rm temp.xml
+while read p; do
+    CName=`echo "$p" | cut -d'.' -f1`
+    OdeUrl="http://oderest.rsl.wustl.edu/live2/?target=mars&query=product&results=m&output=XML&pt=TRDRDDR&iid=CRISM&ihid=MRO&proj=c0&westlon=0&eastlon=360&minlat=-90&maxlat=90&pdsid="
+    wget "$OdeUrl$CName" -O temp.xml > /dev/null 2>&1
+    ELon=($(grep -oP '(?<=Easternmost_longitude>)[^<]+' "temp.xml"))
+    WLon=($(grep -oP '(?<=Westernmost_longitude>)[^<]+' "temp.xml"))
+    if test `echo "$WLon<180 && $ELon>180 && ($WLon-$ELon)<180" | bc -l` -gt 0; then
+        echo "rm $p"
+    fi
+    rm temp.xml
+done < $1
